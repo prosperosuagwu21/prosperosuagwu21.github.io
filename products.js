0352899d5ea52604f69b422d035b6ec71312b22a
prosperosuagwu21 <prosper.osuagwu21@gmail.com>
2026-02-04 21:26:49 +0000

    Add initial products array with sample products

---
+++ products.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        id: 1,
+        name: "Nike Air Force 1",
+        price: 120,
+        image: "https://via.placeholder.com/400",
+        sizes: {
+            "7": 5, "8": 3, "9": 4, "10": 2, "11": 3, "12": 1, "13": 2
+        }
+    },
+    {
+        id: 2,
+        name: "Apple AirPods",
+        price: 159,
+        image: "https://via.placeholder.com/400",
+        sizes: null,
+        stock: 10
+    },
+    {
+        id: 3,
+        name: "Nike Premium Socks",
+        price: 18,
+        image: "https://via.placeholder.com/400",
+        sizes: {
+            "S": 6, "M": 8, "L": 4
+        }
+    }
+];
